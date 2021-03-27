5a1354103b3e2d0f5b3e4af4785f45f672e872c7
Alex Villecco <alexlecco24@gmail.com>
2021-03-27 11:50:11 -0300

    fix axios cors error

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const instanceIpAPI = axios.create({
   baseURL: "http://ip-api.com/json",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 const instanceOpenWeatherMap = axios.create({
